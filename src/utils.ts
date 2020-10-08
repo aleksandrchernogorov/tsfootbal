02d7167bc517d9c0ba74e7a5222421fb75ad37de
aleksandrchernogorov <60900369+aleksandrchernogorov@users.noreply.github.com>
2020-10-09 01:21:57 +0500

    First edit

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export const dateStringToDate = (datestring: string): Date => {
+  const dateParts = datestring.split('/').map((value: string): number => {
+    return parseInt(value);
+  });
+
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
