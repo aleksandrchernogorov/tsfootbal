ef23bc764e9c057443e5215861d7e0bb14b80c1b
aleksandrchernogorov <60900369+aleksandrchernogorov@users.noreply.github.com>
2020-10-10 02:24:33 +0500

    Second commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { read } from 'fs';
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('098 football.csv');
+const reader = new MatchReader('098 football.csv');
 reader.read();
 
 let manUnitedWins = 0;
